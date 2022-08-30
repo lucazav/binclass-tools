486cad738a5adce647852b3b97ef5fa574c3d44e
GretaVilla <gretavilla96@gmail.com>
2022-08-30 12:38:57 +0000

    updated version to 0.2.4 (added compatible python versions)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name="binclass-tools",  # 
     
-    version="0.2.3",  # Required
+    version="0.2.4",  # Required
     description="A set of tools that facilitates the analysis of binary classification problems",  # Optional
     
     long_description=long_description,  # Optional
@@ -31,6 +31,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     
     keywords="binary, classification, confusion, matrix, threshold, plot, precision, recall", 
