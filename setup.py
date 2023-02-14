8437a0aa606a8abda49ce7c08b81bdfecfde22b3
GretaVilla <gretavilla96@gmail.com>
2023-02-14 14:49:36 +0000

    0.3.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name="binclass-tools",  # 
     
-    version="0.3.6",  # Required
+    version="0.3.7",  # Required
     description="A set of tools that facilitates the analysis of binary classification problems",  # Optional
     
     long_description=long_description,  # Optional
